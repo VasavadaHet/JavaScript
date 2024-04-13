3272e8647088d0e9e3343735c9d9a590a9080290
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-13 07:35:33 +0000

    datatypes on day1

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treats all the JS code a newer version
+
+// alert(3+3)  //we are using node here , not browser
+
+console.log(3
++
+3)   // do not do this, make code readability your top priority
+
+console.log("Vasavada");
+
+/*ECMAscript contains all the standardization of js bcz earlier different browsers use to interpret js differently*/
+
+let name = "het"
+let age = 21
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigint => for larger numbers
+//string => ""
+//boolean => true/false
+//null => standalone value/empty value declared
+//undefined => no value assonged to variable
+//symbol =>unique
+//object
+
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  // undefined
+
