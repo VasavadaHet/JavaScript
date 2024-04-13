0842b43365dfb9bdc48be3644a9585281c7b68d2
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-13 13:10:29 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,58 @@ let someNumber =  33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+/*****************Operations**************/
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " het"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);   //complex situation, ans is 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+
+
+// console.log(+true); // 1
+// console.log(+ ""); // 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2    // not readable, do not do this
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
+
+//Prefix and Postfix
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
