9960d3f335891606b9d6e366118db691443b30a8
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-13 14:28:34 +0000

    stack and heap

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -33,3 +33,37 @@ const myfunction = function(){
 
 
 console.log(typeof bigNumber);
+
+
+/* ******************** Memories *************** */
+
+// Stack (Primitives), Heap (Non Primitives)
+// In stack you get copy of the variable declared
+// In heap you get reference of that
+
+let myYoutubeName = "vasavada.com"
+
+let anotherName = myYoutubeName
+
+anotherName = "chaiaurcode"
+
+console.log(anotherName); // chaiaurcode
+console.log(myYoutubeName);  // vasavada.com
+/*this means that when you assinged the value of myYoutubeName to anotherName , 
+in real a copy of that value was created and assigned, 
+and then the change which we made was change in copy
+*/
+
+let user1 = {
+    email: "user@google.com",
+    upi: "hv@ybl"
+}
+
+let user2 = user1
+/* here you will get the same reference 
+if we change value in one, the value will change n another as well*/
+
+user2.email = "het@google.com"
+
+console.log(user1.email); // het@google.com
+console.log(user2.email); // het@google.com
