11f879fa7214640b9857c07cb66ca45825f468a0
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-13 10:09:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  // NaN
+
+// let value = null
+
+// let finalAnswer = Number(value)
+// console.log(typeof finalAnswer)
+// console.log(finalAnswer);   // 0 
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIN = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIN);
+
+
+// 1 => true; 0 => false
+// "" => false; "het" => true
+
+
+
+let someNumber =  33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
