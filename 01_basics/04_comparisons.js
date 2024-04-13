75dbf8d0b5bb83a2bc5bfc26841b2ad7447e5abb
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-13 13:39:13 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 ==1);
+// console.log(2 != 1);
+
+// console.log("2" > 1 );
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0 );
+// console.log(null >= 0);  //comparisons convert null to a number treatng it as 0 , that is why null >0 false  and null >= 0 ture
+
+// console.log(undefined == 0);  // same problem as null but is gives false for all
+
+
+// ===  strictly check
+
+console.log("2" === 2); //it will compare the datatypes
+ 
