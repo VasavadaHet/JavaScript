a1b7b346c3dee0806eaa096d691d8818c13b257e
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-14 04:21:08 +0000

    numbers,maths,random number

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,44 @@
+// const score = 400
+// console.log(score);  // 400
+
+// const balance = new Number(100)
+// console.log(balance);  // [Number : 100]
+
+// console.log(balance.toString().length); //3
+// console.log(balance.toFixed(2));  // 100.00
+
+// const otherNumber = 123.8956
+// console.log(balance.toPrecision(3));  //124
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
+
+
+
+/********************* Maths *******************/
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.3)); // 5
+// console.log(Math.floor(4.3)); // 4
+// console.log(Math.min(2,6,1,34)); // 1
+// console.log(Math.max(2,5,3,8)); // 8
+
+//Random
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1); //num between 1 to 10
+console.log((Math.floor(Math.random()*10)) + 1); // integer between 1 to 10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  // integer between 10 and 20
+
+
+
+
+
+
+
