4dad4899685b1c404d34e971d87fde9547836d30
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-17 04:28:42 +0000

    objects part 2 completed

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -18,4 +18,33 @@ const reuglarUser = {
     }
 }
 
-console.log(reuglarUser.fullname.userfullname.firstname)
+// console.log(reuglarUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+//combining two objects
+// const obj3 = Object.assign({}, obj1, obj2) //can also combine more than two objects.....
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "v@email.com"
+    }
+]
+
+// console.log(users[1].email)
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser)); // will return array of keys in an object.....
+// console.log(Object.values(tinderUser));  // will return array of values in an object.....
+// console.log(Object.entries(tinderUser)); //every key and value will be in an array...
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check whether the given property is available or not
