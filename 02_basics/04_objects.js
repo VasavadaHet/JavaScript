a88660227fe2b704235bd5b3b49a2b2ceff45e23
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-17 04:51:17 +0000

    objects fully compeleted and json, apis intro done.....

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,4 +47,46 @@ const users = [
 // console.log(Object.values(tinderUser));  // will return array of values in an object.....
 // console.log(Object.entries(tinderUser)); //every key and value will be in an array...
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check whether the given property is available or not
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check whether the given property is available or not
+
+
+// const course = {
+//     coursename: " Js in hindi",
+//     price: "999",
+//     courseTeacher: "hitesh"
+// }
+
+// course.courseTeacher
+ 
+//de-structuring of an object
+// const {courseTeacher: instructor} = course
+
+// console.log(courseTeacher);
+// console.log(instructor);
+
+
+//Example of react (de-structuring)
+// const navbar = ({company}) =>{
+
+// }
+
+// navbar(company = "hitesh")
+
+
+//APIs intro and JSON
+
+// {
+//     "name":"hitesh",
+//     "coursename": "js in hindi",
+//     "price":"free"
+// }
+// this is how json looks
+
+
+// sometimes you get api in array as well
+// [
+//     {},
+//     {} 
+// ]
+
+//randomuser me api , json formatter (just to explore )
