c248c73a89eb0f843d403655cf91670320c1bc83
VasavadaHet <105774119+VasavadaHet@users.noreply.github.com>
2024-04-18 12:58:52 +0000

    scopes part 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,28 @@ let a = 300
 if(true){
     let a = 10
     const b = 20 //block scope
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     // var c= 30
 }
 
 // console.log(a); ReferenceError: a is not defined
 // console.log(b); ReferenceError: b is not defined
 // console.log(c); //30
-console.log(a);
+// console.log(a);
+
+
+//Nested Scope
+function one(){
+
+    const username = "vasavada"
+
+    function two(){
+        const website= "github"
+        console.log(username);   // no error, chotte bacche bade bcche ki icecream le sakte hai
+    }
+
+    // console.log(website);  ReferenceError: website is not defined , bade bacche chotte bache ki icecream nahi  maang sakte
+    two()
+}
+
+// one()
